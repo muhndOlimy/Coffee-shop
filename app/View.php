9eeb4bc812abaf4f316851e6eff648df5748bc05
mouEsam <drsh_201122@yahoo.com>
2025-01-28 23:27:06 +0200

    initial php server setup

---
+++ app/View.php
@@ -0,0 +1,15 @@
+<?php
+
+readonly class View
+{
+    public function __construct(private string $viewPath) {}
+
+    public function render(): void {
+        require __DIR__ . "/../views/" . $this->viewPath;
+    }
+
+    public static function renderView(string $viewPath): void
+    {
+        new View($viewPath)->render();
+    }
+}
